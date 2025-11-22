91f9540aead32578b4663d2eeeb02d41a4bf7a99
Ramazan <ramazanaydinli94@gmail.com>
2025-11-22 22:48:13 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  // Statik dışa aktarmayı etkinleştirir.
+  output: 'export',
+  
+  // Derleme çıktı klasörünü, GitHub Pages'ın yayımlaması için doğru yer olan 'out' olarak ayarlar.
+  distDir: 'out', 
+
+  // Statik yayınlama için Next.js Image bileşeninin optimize edilmesini devre dışı bırakır.
+  images: {
+    unoptimized: true,
+  },
+
+  /* Mevcut diğer ayarlarınızı buraya ekleyin */
+  reactCompiler: true, 
 };
 
 export default nextConfig;
